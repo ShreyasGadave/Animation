569fda2a56e693e0a131fb03a862c32f226b883c
shreyasgadave <shreyasgadave777@gmail.com>
2024-10-23 20:58:05 +0530

    animation left

---
+++ src/components/Cards.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Cards() {
+  return (
+    <div>
+      shreyas
+    </div>
+  )
+}
+
+export default Cards
--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
 
 function Featured() {
   return (
-    <div>
-      shreyas
+    <div className="w-full py-20">
+      <div className="w-full px-20 border-b-[1px] border-zinc-700 pb-20">
+        <h1 className="text-6xl font-['Neue Montreal'] tracking-tight ">
+          Featured Project
+        </h1>
+      </div>
+      <div className="px-20">
+        <div className="cards w-full flex gap-10 mt-10">
+          <div className="heading  absolute bg-red-600"></div>
+          <div className="cardcontainer relative  w-1/2 h-[75vh]">
+            <h1 className="absolute left-full text-[#CDEA68] -translate-x-1/2 -translate-y-1/2   top-1/2 z-[9] text-8xl">
+            {"FYDE".split('').map((item, index)=> <span>{item}</span>)}
+            </h1>
+            <div className=" card w-full h-full rounded-xl overflow-hidden">
+              <img
+                className="w-full h-full bg-cover"
+                src="https://ochi.design/wp-content/uploads/2023/10/Fyde_Illustration_Crypto_2-1326x1101.png"
+                alt=""
+              />
+            </div>
+          </div>
+          <div className="cardcontainer relative w-1/2 h-[75vh] ">
+            <h1 className="absolute right-full text-[#CDEA68] translate-x-1/2 -translate-y-1/2 top-1/2  z-[9] text-8xl   ">
+           {"VISE".split('').map((item, index)=> <span>{item}</span>)}
+            </h1>
+            <div className="card w-full h-full rounded-xl overflow-hidden">
+              <img
+                className="w-full h-full bg-cover"
+                src="https://ochi.design/wp-content/uploads/2022/09/Vise_front2-1326x1101.jpg"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Featured
+export default Featured;
